3f6d8b79a0cb28e97d22738743d7337585747208
Andrey Povstyanko <andrey.povstyanko@m-files.com>
2026-04-21 20:00:12 +0500

    changed api

--- server/src/schemas.ts
+++ server/src/schemas.ts
@@ -14,7 +14,7 @@ export const ItemSchema = registry.register(
   z.object({
     id: z.number(),
     item_name: z.string(),
-    item_price: z.number(),
+    item_super_price: z.number(),
     created_at: z.string().nullable(),
   }),
 );
