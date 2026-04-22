c9e2e4dda1f70c1d897833a1efcb18284564c14e
raldin14 <raldin.hidalgo@gmail.com>
2026-04-22 17:45:16 -0500

    new branch matching be

--- server/src/schemas.ts
+++ server/src/schemas.ts
@@ -26,7 +26,7 @@ export const OrderSchema = registry.register(
     id: z.number(),
     user_id: z.string().uuid().nullable(),
     order_item_name: z.string(),
-    order__Date: z.string().nullable(),
+    order__date: z.string().nullable(),
   }),
 );
 
